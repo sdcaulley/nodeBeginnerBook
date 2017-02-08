8522931a9042c6b8494d5d14e54a9c911c8b10ca
sdcaulley <stephanie.caulley@gmail.com>
2017-02-08 06:12:44 -0800

    added request handlers to object in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const server = require('./server');
 const router = require('./router');
+const requestHandlers = require('./request-handlers');
+
+const handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
 
 server.start(router.route);
