32e698981e98382b9fc5957590b8c64429341b8c
sdcaulley <stephanie.caulley@gmail.com>
2017-02-08 06:01:26 -0800

    added router to index.js and injected it into server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(req, res) {
         let pathname = url.parse(req.url).pathname;
-        console.log('Request received');
+        console.log('Request for ' + pathname + ' received.');
+
+        route(pathname);
+
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('Hello World');
         res.end();
