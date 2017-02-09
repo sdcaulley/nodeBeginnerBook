f8afc8703a3f2db7b298208c44bab96bb112dab5
sdcaulley <stephanie.caulley@gmail.com>
2017-02-09 05:42:15 -0800

    example of blocking process

--- request-handlers.js
+++ request-handlers.js
@@ -1,5 +1,12 @@
 function start() {
     console.log('Request handler "Start" was called.');
+
+    function sleep(miliSeconds) {
+        let startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + miliSeconds);
+    }
+
+    sleep(10000);
     return 'Hello Start!';
 }
 
