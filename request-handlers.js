6e8c533b6b95b9b2eedca187615f5eea58f3cef4
sdcaulley <stephanie.caulley@gmail.com>
2017-02-09 06:27:08 -0800

    basic form for post requests

--- request-handlers.js
+++ request-handlers.js
@@ -1,13 +1,20 @@
-const exec = require('child_process').exec;
-
 function start(res) {
     console.log('Request handler "Start" was called.');
 
-    exec('ls -lah', (error, stdout, stderr) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write(stdout);
-        res.end();
-    });
+    const body = '<html>' + '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(body);
+    res.end();
 }
 
 function upload(res) {
